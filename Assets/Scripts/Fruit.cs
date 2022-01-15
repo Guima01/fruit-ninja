6f1ac8331b48e12d9080a79e9024fb48f9e49ad3
Guilherme Marques <guilhermemarques1@ice.ufjf.br>
2022-01-15 20:14:07 -0300

    Cortando fruta

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Fruit : MonoBehaviour
 {
+    public GameObject Sliced;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +20,10 @@ public class Fruit : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D other) {
         if(other.gameObject.tag == "Knife"){
             Destroy(gameObject);
+            GameObject appleLeft = Instantiate(Sliced, transform.position + new Vector3 (-0.2f, 0, 0), Quaternion.identity);
+            appleLeft.transform.Rotate(0, -70, 0);
+            GameObject appleRight = Instantiate(Sliced, transform.position + new Vector3 (0.2f, 0, 0), Quaternion.identity);
+            appleRight.transform.Rotate(0, 70, 0);
         }
     }
 }
