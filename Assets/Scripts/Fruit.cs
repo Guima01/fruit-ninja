c3d40dc8dbc21a1f1374be9569cc4502f9db3372
Guilherme Marques <guilhermemarques1@ice.ufjf.br>
2022-01-16 23:30:32 -0300

    Adicionando força a fruta

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class Fruit : MonoBehaviour
 {
     public GameObject Sliced;
+    public float jumpHeight = 5.0f;
+    public bool applyForce = true;
+    int num;
     // Start is called before the first frame update
     void Start()
     {
-        
+        var rand = new System.Random();
+        num = rand.Next(-7, 7);
+        transform.position = new Vector3(num,-6.0f,0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.y < 0.2f && applyForce){
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, jumpHeight), ForceMode2D.Force); 
+        }
+        else {
+            applyForce = false;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
